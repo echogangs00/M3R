f952689e04f9687055c0f23254f53cd7d20d7602
echogangs00 <echogangs99@outlook.com>
2016-12-17 20:09:41 -0500

    Copied MSR

---
+++ RecyclerViewTest2/app/src/main/java/trashey/recyclerviewtest/ui/ListActivity.java
@@ -0,0 +1,123 @@
+package trashey.recyclerviewtest.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import trashey.recyclerviewtest.R;
+import trashey.recyclerviewtest.adapter.DerpAdapter;
+import trashey.recyclerviewtest.model.DerpData;
+import trashey.recyclerviewtest.model.ListItem;
+
+public class ListActivity extends AppCompatActivity implements DerpAdapter.ItemClickCallback{
+
+    private static final String BUNDLE_EXTRAS = "BUNDLE_EXTRAS";
+    private static final String EXTRA_QUOTE = "EXTRA_QUOTE";
+    private static final String EXTRA_ATTR = "EXTRA_ATTR";
+
+    private RecyclerView recyclerView;
+    private DerpAdapter adapter;
+    private ArrayList listData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        listData = (ArrayList)DerpData.getListData();
+        recyclerView = (RecyclerView)findViewById(R.id.rec_list);
+        //LayoutManager: GridLayoutManager or StaggeredGridLayoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new DerpAdapter(listData, this);
+        recyclerView.setAdapter(adapter);
+        adapter.setItemClickCallback(this);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(createHelperCallback());
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        Button addItem = (Button)findViewById(R.id.btn_add_item);
+        addItem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addItemToList();
+            }
+        });
+
+    }
+
+    private ItemTouchHelper.Callback createHelperCallback() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                          RecyclerView.ViewHolder target) {
+                        moveItem(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+                        return true;
+                    }
+
+                    @Override
+                    public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                        deleteItem(viewHolder.getAdapterPosition());
+                    }
+                };
+        return simpleItemTouchCallback;
+    }
+    private void addItemToList() {
+        ListItem item = DerpData.getRandomListItem();
+        listData.add(item);
+        adapter.notifyItemInserted(listData.indexOf(item));
+    }
+
+    private void moveItem(int oldPos, int newPos) {
+
+        ListItem item = (ListItem) listData.get(oldPos);
+        listData.remove(oldPos);
+        listData.add(newPos, item);
+        adapter.notifyItemMoved(oldPos, newPos);
+    }
+
+    private void deleteItem(final int position) {
+        listData.remove(position);
+        adapter.notifyItemRemoved(position);
+    }
+
+    @Override
+    public void onItemClick(int p) {
+        ListItem item = (ListItem) listData.get(p);
+
+        Intent i = new Intent(this,DetailActivity.class);
+
+        Bundle extras = new Bundle();
+        extras.putString(EXTRA_QUOTE, item.getTitle());
+        extras.putString(EXTRA_ATTR, item.getSubTitle());
+
+        i.putExtra(BUNDLE_EXTRAS, extras);
+        startActivity(i);
+    }
+
+    @Override
+    public void onSecondaryIconClick(int p) {
+        ListItem item = (ListItem) listData.get(p);
+
+        if(item.isFavourite()){
+            item.setFavourite(false);
+        }else {
+            item.setFavourite(true);
+        }
+
+        adapter.notifyDataSetChanged();
+
+    }
+}
+
+
